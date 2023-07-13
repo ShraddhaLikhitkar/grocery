8e60df2fae4591747203e91ab6a25da89d1ee75b
ShraddhaLikhitkar <117911876+ShraddhaLikhitkar@users.noreply.github.com>
2023-07-13 11:37:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let searchFrom = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchFrom.classList.toggle('active');
+    navbar.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchFrom.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchFrom.classList.remove('active');
+    shoppingCart.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchFrom.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll =() =>{
+    navbar.classList.remove('active');
+    searchFrom.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+   loop:true,
+    spaceBetween: 10,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+    0: {
+        slidesPerView:1,
+    },
+    768: {
+        slidesPerView:2,
+    },
+    1024: {
+        slidesPerView:3,
+    },
+    },
+});
+var swiper = new Swiper(".review-slider", {
+   loop:true,
+    spaceBetween: 10,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction:false,
+    },
+    breakpoints: {
+    0: {
+        slidesPerView:1,
+    },
+    768: {
+        slidesPerView:2,
+    },
+    1024: {
+        slidesPerView:3,
+    },
+    },
+});
